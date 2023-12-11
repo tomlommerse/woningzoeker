3cc8cc3512543a5ca9c5aa5a730ff30be492f1b0
tomlommerse <tomlommerse2001@gmail.com>
2023-12-11 13:36:46 +0100

    components setup

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+// src/components/HomePage.js
+
+import React from 'react';
+
+const HomePage = () => {
+    return (
+    <main>
+        <h1>hallo</h1>
+    </main>
+    );
+};
+
+export default HomePage;
---
+++ src/components/TopBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopBar = () => {
+    return (
+        <div>
+            <img src="../img/woningzoeker.png" alt="logo van de woningzoeker"/>
+        </div>
+    );
+};
+
+export default TopBar;
