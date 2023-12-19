4ca8fa3d4cff3dc5bc503d6592953134a919b24e
ImrevdV <ImrevdV>
2023-12-20 00:32:50 +0100

    mapmovement (arrowbuttons)

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/map.css';
-import Button from './Woningpagina/button.jsx';
+import { ArrowBigLeft, ArrowBigRight } from "lucide-react"
 
 import TestMap from '../assets/TestMap.jpg';
 
@@ -8,9 +8,29 @@ const MAP = TestMap
 
 
 function MapPage() {
+  function mapLeft(){
+    let img = document.getElementById("js-mapimg");
+
+    img.classList.remove("mapImgRight");
+    img.classList.add("mapImgLeft");
+  }
+  function mapRight(){
+    let img = document.getElementById("js-mapimg");
+
+    img.classList.remove("mapImgLeft");
+    img.classList.add("mapImgRight");
+  }
   return (
     <section className='mapSection'>
-        <img className='mapImg' src={MAP} alt="" />
+      <div className='mapImgContainer'>
+        <img id='js-mapimg' className='mapImg' src={MAP} alt="" />
+        <button onClick={mapLeft} className="mapImgNavbutton" style={{left: 0}}>
+            <ArrowBigLeft/>
+        </button>
+        <button onClick={mapRight} className="mapImgNavbutton" style={{right: 0}}>
+            <ArrowBigRight/>
+        </button>
+      </div>
     </section>
   )
 }
