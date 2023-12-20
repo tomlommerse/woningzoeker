ff7be88b769515337ed26ce5be7d84e9233e2843
tomlommerse <tomlommerse2001@gmail.com>
2023-12-20 09:56:56 +0100

    home routing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <TopBar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/detailpage" element={<DetailPage />} />
         <Route path="/kaart" element={<MapPage />} />
       </Routes>
