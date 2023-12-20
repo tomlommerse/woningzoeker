54ba1d2660c08edbd19b37ee769f236ea37359d8
tomlommerse <tomlommerse2001@gmail.com>
2023-12-20 10:01:30 +0100

    routing detailpage fix

--- src/components/cards/HomeCard.jsx
+++ src/components/cards/HomeCard.jsx
@@ -10,7 +10,7 @@ function HomeCard({ home, price, type, parking, m2, room_count }) {
   const room_countTag = `${room_count} kamers`;
 
   return (
-      <Link className='homeCard' to="/detailpagina">
+      <Link className='homeCard' to="/detailpage">
         <div className="imgArea">
           <img id='img' src={Test} alt='house' />
           <p id='adres'>{home}</p>
