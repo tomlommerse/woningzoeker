127108754dafb11802f644ff8d0a9b0f7ef044af
Quinten <qdamsma@gmail.com>
2024-01-09 14:47:48 +0100

    Slider voor filters

--- src/components/Woningpagina/filter/ListPageMainFilter.jsx
+++ src/components/Woningpagina/filter/ListPageMainFilter.jsx
@@ -1,14 +1,15 @@
 // src/components/Woningpagina/filter/ListPageMainFilter.js
 import React from 'react';
+import '../../../styles/filterlistpage.css';
 
 const ListPageMainFilter = ({ type, isActive, onClick }) => {
     return (
-    <div
-        className={`filter-button ${isActive ? 'active' : ''}`}
-        onClick={() => onClick(type)}
-    >
-        {type}
-    </div>
+        <div className={`filter-slider`}>
+            <div className={`filter-button ${isActive ? 'active' : ''}`}
+                onClick={() => onClick(type)}>
+                {type}
+            </div>
+        </div>
     );
 };
 
