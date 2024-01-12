fdb2fcc254cd342417ba89bc8e761cd60a2968b0
tomlommerse <tomlommerse2001@gmail.com>
2024-01-12 17:59:08 +0100

    save homepage to localstorage

--- src/views/StartPage.jsx
+++ src/views/StartPage.jsx
@@ -6,23 +6,28 @@ import pic from '../assets/startpic.jpg';
 
 function StartPage() {
     const [selectedType, setSelectedType] = useState('');
+    const [minPrice, setMinPrice] = useState('');
+    const [maxPrice, setMaxPrice] = useState('');
 
-    const handleSearch = () => {
-        console.log('Selected Type:', selectedType);
-        // Add your logic here for handling the selected type
+    const search = () => {
+        localStorage.setItem('selectedType', selectedType);
+        localStorage.setItem('minPrice', minPrice);
+        localStorage.setItem('maxPrice', maxPrice);
+
+        console.log('LocalStorage:', localStorage);
     };
 
     return (
         <main>
             <img id='startpic' src={pic} alt='house' />
             <section className='searchbox'>
-            <h2>type huis</h2>
+                <h2>type huis</h2>
                 <Dropdown dataKey="type" onChange={(value) => setSelectedType(value)} />
                 <h2>Minimumprijs</h2>
-                <Dropdown dataKey="price" roundBy={100000} roundDirection="down" onChange={(value) => setSelectedType(value)} />
+                <Dropdown dataKey="price" roundBy={100000} roundDirection="down" onChange={(value) => setMinPrice(value)} />
                 <h2>Maxmumprijs</h2>
-                <Dropdown dataKey="price" roundBy={100000} roundDirection="up" onChange={(value) => setSelectedType(value)} />
-                <button className='zoekButton' onClick={handleSearch}>
+                <Dropdown dataKey="price" roundBy={100000} roundDirection="up" onChange={(value) => setMaxPrice(value)} />
+                <button className='zoekButton' onClick={search}>
                     Zoek
                 </button>
             </section>
