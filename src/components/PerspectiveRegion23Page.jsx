357d5de402375bbd0aa8e676224d2bbf8e0468ca
ImrevdV <ImrevdV>
2024-01-15 12:50:51 +0100

    added popupcard

--- src/components/PerspectiveRegion23Page.jsx
+++ src/components/PerspectiveRegion23Page.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import '../styles/3d.css';
 import { Link } from 'react-router-dom';
-import HomeCard from './cards/HomeCard';
+import '../styles/card.css';
+import Test from '../assets/Test_2.jpg';
+import Tags from './cards/tags';
 
 import jsonData from '../assets/wonen-in-de-kuil.json';
 
@@ -20,20 +22,26 @@ function PerspectiveRegion23Page() {
       <svg version="1.1" width="45%" viewBox="626.3302583026234 56.3302583025943 668.3394833947532 668.3394833948114" id="svgParentElement">
         <image width="1920" height="780" href="https://backend.woningzoekerheijmans.nl/storage/211/woningzoeker_zijaanzicht_dijk.jpg"></image>
         {plothotspots.map((hotspot) => (
-          <polygon key={hotspot.svg} onClick={() => changeCurrentPlot(hotspot)} points={hotspot.svg} fill={plots.find((p) => p.id === hotspot.entity_id).status === 'verkocht' ? 'red' : plots.find((p) => p.id === hotspot.entity_id).status === 'in-optie' ? 'orange' : 'green'} fillOpacity="0.5" opacity="0.9" width="1" strokeOpacity="0" stroke="white" strokeWidth="3"></polygon>
+          <polygon key={hotspot.svg} onClick={() => changeCurrentPlot(hotspot)} points={hotspot.svg} fill={plots.find((p) => p.id === hotspot.entity_id).status === 'verkocht' ? '#FF0000' : plots.find((p) => p.id === hotspot.entity_id).status === 'in-optie' ? '#FFA500' : '#04B900'} fillOpacity="0.5" opacity="0.9" width="1" strokeOpacity="0" stroke="white" strokeWidth="3"></polygon>
         ))}
       </svg>
       {currentPlot && (
-        <HomeCard
-          key={currentPlot.id}
-          home={currentPlot.number}
-          price={currentPlot.price}
-          type={currentPlot.type}
-          parking={currentPlot.parking_count}
-          m2={currentPlot.living_surface}
-          room_count={currentPlot.room_count}
-          status={currentPlot.status}
-        />
+        <div className='homeCard plotPopup'>
+          <div className="imgArea">
+            <img id='img' src={Test} alt='house' />
+            <p id='adres'>{currentPlot.number}</p>
+            <p id='price' style={{ backgroundColor: (currentPlot.status === 'verkocht' ? '#FF0000' : currentPlot.status === 'in-optie' ? '#FFA500' : '#04B900') }}>
+              €{currentPlot.price}
+            </p>
+          </div>
+          <div className='tagList'>
+            <Tags tagname={currentPlot.type} />
+            <Tags tagname={`${currentPlot.parking_count} parkeerplaatsen`} />
+            <Tags tagname={`${currentPlot.living_surface} m²`} />
+            <Tags tagname={`${currentPlot.room_count} kamers`} />
+            <Tags tagname={currentPlot.status} />
+          </div>
+        </div>
       )}
     </section>
   )
